ffa277813e43f03b0678ef4b8119beb9aabbe791
robyf <roberto@federica.rf.priv>
2016-09-09 19:34:30 +0300

    Using the configuration from config closure

--- src/main/groovy/net/robyf/gradle/jarinjar/JarInJarPlugin.groovy
+++ src/main/groovy/net/robyf/gradle/jarinjar/JarInJarPlugin.groovy
@@ -2,22 +2,30 @@ package net.robyf.gradle.jarinjar
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.UnknownConfigurationException
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.internal.artifacts.publish.ArchivePublishArtifact
-import org.gradle.api.internal.plugins.DefaultArtifactPublicationSet
-import org.gradle.language.base.plugins.LifecycleBasePlugin;
 import org.gradle.api.internal.java.JavaLibrary
+import org.gradle.api.internal.plugins.DefaultArtifactPublicationSet
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.language.base.plugins.LifecycleBasePlugin
 
 class JarInJarPlugin implements Plugin<Project> {
 
     void apply(Project project) {
+        //project.getPluginManager().apply(JavaPlugin.class);
+    
         def extension = project.extensions.create('executableJar', JarInJarPluginExtension)
-        extension.configuration = project.configurations.runtime
+        try {
+            extension.configuration = project.configurations.runtime
+        } catch(UnknownConfigurationException e) {
+        } catch(MissingPropertyException e) {
+        }
         
         project.afterEvaluate {
             project.jar {
-                from project.configurations.runtime
+                from project.executableJar.configuration
                 
                 def pluginJarFile = project.buildscript.configurations.classpath.find { it.getName().startsWith('gradle-jarinjar-plugin') && it.getName().endsWith('jar') }
                 from project.zipTree(pluginJarFile.getAbsolutePath()).matching {
